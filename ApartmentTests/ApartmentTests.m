8282b47322bdaf82778e82d91692991303486829
apple <164757979@qq.com>
2015-12-28 10:47:01 +0800

    Initial Commit

---
+++ ApartmentTests/ApartmentTests.m
@@ -0,0 +1,39 @@
+//
+//  ApartmentTests.m
+//  ApartmentTests
+//
+//  Created by apple on 15/12/28.
+//  Copyright © 2015年 zcp. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ApartmentTests : XCTestCase
+
+@end
+
+@implementation ApartmentTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
